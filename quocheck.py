e52ba7375f665bfcded0dc152393c2729b491577
jackkar <jkarpichev@gmail.com>
2021-04-01 21:50:15 +0300

    Workaround to fix import hell untill a better idea manifests

--- quocheck.py
+++ quocheck.py
@@ -1,8 +1,30 @@
-import json
 import random
 
 
-FNAME = 'quotes.json'
+QUOTES = {
+    "Sometimes Ill start a sentence and I dont even know where its going. I just hope I find it along the way.": "Michael Scott",
+    "I talk a lot, so Ive learned to tune myself out.": "Kelly Kapoor",
+    "Would I rather be feared or loved? Easy. Both. I want people to be afraid of how much they love me.": "Michael Scott",
+    "Im not superstitious, but I am a little stitious.": "Michael Scott",
+    "If I dont have some cake soon, I might die.": "Stanley Hudson",
+    "The worst thing about prison was the dementors.": "Michael Scott",
+    "Identity theft is not a joke, Jim! Millions of families suffer every year.": "Dwight Schrute",
+    "Today, smoking is going to save lives.": "Dwight Schrute",
+    "I am running away from my responsibilities. And it feels good.": "Michael Scott",
+    "I just want to lie on the beach and eat hot dogs. Thats all Ive ever wanted.": "Kevin Malone",
+    "If I were buying my coffin, I would get one with thicker walls so you couldnt hear the other dead people.": "Dwight Schrute",
+    "And I knew exactly what to do. But in a much more real sense, I had no idea what to do.": "Michael Scott",
+    "Me think, why waste time say lot word, when few word do trick.": "Kevin Malone",
+    "I dont hate it. I just dont like it at all and its terrible.": "Michael Scott",
+    "Who is Justice Beaver?": "Dwight Schrute",
+    "Rit-dit-dit-do-doo!": "Andy Bernard",
+    "I am a black belt in gift wrapping.": "Jim Halpert",
+    "I kinda know what its like to be in commercials. My nickname in high school used to be Kool-Aid Man.": "Kevin Malone",
+    "I mean, Im not a slut, but who knows.": "Kelly Kapoor",
+    "Tell him to call me ASAP as possible.": "Michael Scott",
+    "I say dance, they say, How high?": "Michael Scott"
+}
+
 
 class Quocheck:
     """
@@ -11,8 +33,7 @@ class Quocheck:
 
     def __init__(self):
         # load the quotes
-        with open(FNAME, 'r') as quo:
-            self.quotes = json.load(quo)
+        self.quotes = QUOTES
     
     def get_quote(self):
         quote = random.choice(list(self.quotes.keys()))
